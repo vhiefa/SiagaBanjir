5b2f3f5854191702b7bcbbe08b21a63490dbf4d4
Faqih Arifian <faqiharifianaji@gmail.com>
2015-08-14 19:19:18 +0700

    create database contract called FloodContract

---
+++ app/src/main/java/com/digitcreativestudio/siagabanjir/data/FloodContract.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.digitcreativestudio.siagabanjir.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Defines table and column names for the weather database.
+ */
+public class FloodContract {
+
+    public static final String CONTENT_AUTHORITY = "com.digitcreativestudio.siagabanjir";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_FLOOD = "flood";
+
+    public static final class MakulEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FLOOD).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FLOOD;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FLOOD;
+
+        public static final String TABLE_NAME = "flood";
+
+        public static final String COLUMN_TIME = "time";
+        public static final String COLUMN_CAPTION = "caption";
+        public static final String COLUMN_LATITUDE = "latitude";
+        public static final String COLUMN_LONGITUDE = "longitude";
+        public static final String COLUMN_PHOTO = "photo";
+
+        public static Uri buildFloofUri(String lat, String lon) {
+            return CONTENT_URI.buildUpon().appendPath(lat).appendPath(lon).build();
+        }
+
+        public static String getLatitude(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static String getLongitude(Uri uri){
+            return uri.getPathSegments().get(2);
+        }
+
+    }
+
+}
