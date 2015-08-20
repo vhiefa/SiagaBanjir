347fb2e7dc1dca9486e8e96bfc09b6b717742cad
Afifatul Mukaroh <afifatul.mukaroh@gmail.com>
2015-08-20 20:07:55 +0700

    Update FloodDbHelper.java

--- app/src/main/java/com/digitcreativestudio/siagabanjir/data/FloodDbHelper.java
+++ app/src/main/java/com/digitcreativestudio/siagabanjir/data/FloodDbHelper.java
@@ -41,13 +41,16 @@ public class FloodDbHelper extends SQLiteOpenHelper {
 
         final String SQL_CREATE_FLOOD_TABLE = "CREATE TABLE " + FloodEntry.TABLE_NAME + " (" +
 
-                FloodEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                FloodEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + //id untuk database lokal
 
+                FloodEntry.COLUMN_FLOOD_ID + " TEXT NOT NULL, " + //id dari database server
                 FloodEntry.COLUMN_TIME + " TEXT NOT NULL, " +
                 FloodEntry.COLUMN_CAPTION + " TEXT NOT NUll, " +
                 FloodEntry.COLUMN_LATITUDE + " REAL NOT NULL, " +
                 FloodEntry.COLUMN_LONGITUDE + " REAL NOT NULL, " +
                 FloodEntry.COLUMN_PHOTO + " TEXT NOT NULL );";
+                
+                "UNIQUE (" + FloodEntry.COLUMN_FLOOD_ID + ") ON CONFLICT REPLACE);"; //agar data dengan id dr server yang sama tidak menduplikat di database lokal
 
         sqLiteDatabase.execSQL(SQL_CREATE_FLOOD_TABLE);
     }
