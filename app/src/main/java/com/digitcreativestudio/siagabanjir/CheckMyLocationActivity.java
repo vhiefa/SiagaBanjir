f37843d69b99c61ad2f263f1968d11c93f8d6a2a
Afifatul Mukaroh <afifatul.mukaroh@gmail.com>
2015-08-22 08:44:38 +0700

    add map

--- app/src/main/java/com/digitcreativestudio/siagabanjir/CheckMyLocationActivity.java
+++ app/src/main/java/com/digitcreativestudio/siagabanjir/CheckMyLocationActivity.java
@@ -21,6 +21,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.digitcreativestudio.siagabanjir.utils.MyLocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,6 +42,7 @@ public class CheckMyLocationActivity extends ActionBarActivity {
     private Criteria criteria;
     Location location;
     TextView lokasisaya;
+    private GoogleMap googleMap;
 
     Context context;
     Double latitude = -6.166894, longitude = 106.861803; //hanya untuk default (tes), nanti ini akan keganti dengan current lat long si user
@@ -44,6 +51,102 @@ public class CheckMyLocationActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_my_location);
+
+        try {
+            // Loading map
+            initilizeMap();
+
+            // Changing map type
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            // googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            // googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            // googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+            // googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+            // Showing / hiding your current location
+            googleMap.setMyLocationEnabled(true);
+
+            // Enable / Disable zooming controls
+            googleMap.getUiSettings().setZoomControlsEnabled(false);
+
+            // Enable / Disable my location button
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+            // Enable / Disable Compass icon
+            googleMap.getUiSettings().setCompassEnabled(true);
+
+            // Enable / Disable Rotate gesture
+            googleMap.getUiSettings().setRotateGesturesEnabled(true);
+
+            // Enable / Disable zooming functionality
+            googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+			/*
+			double latitude = 17.385044;
+			double longitude = 78.486671;
+
+			// lets place some 10 random markers
+			for (int i = 0; i < 10; i++) {
+				// random latitude and logitude
+				double[] randomLocation = createRandLocation(latitude,
+						longitude);
+
+				// Adding a marker
+				MarkerOptions marker = new MarkerOptions().position(
+						new LatLng(randomLocation[0], randomLocation[1]))
+						.title("Hello Maps " + i);
+
+				Log.e("Random", "> " + randomLocation[0] + ", "
+						+ randomLocation[1]);
+
+				// changing marker color
+				if (i == 0)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+				if (i == 1)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+				if (i == 2)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+				if (i == 3)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+				if (i == 4)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+				if (i == 5)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+				if (i == 6)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+				if (i == 7)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+				if (i == 8)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+				if (i == 9)
+					marker.icon(BitmapDescriptorFactory
+							.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+
+				googleMap.addMarker(marker);
+
+				// Move the camera to last position with a zoom level
+				if (i == 9) {
+					CameraPosition cameraPosition = new CameraPosition.Builder()
+							.target(new LatLng(randomLocation[0],
+									randomLocation[1])).zoom(15).build();
+
+					googleMap.animateCamera(CameraUpdateFactory
+							.newCameraPosition(cameraPosition));
+				}
+			}*/
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Button checkMyLocation = (Button) findViewById(R.id.checkmylocation);
         lokasisaya = (TextView) findViewById(R.id.lokasisaya);
@@ -196,4 +299,34 @@ public class CheckMyLocationActivity extends ActionBarActivity {
             }
         }
     }
+
+    /**
+     * function to load map If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map1)).getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    //untuk memanggil googleMap.setOnMyLocationChangeListener(myLocationChangeListener);
+    //tapi nanti marker redundan jd harus dihapus
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+           Marker mMarker = googleMap.addMarker(new MarkerOptions().position(loc));
+            if(googleMap != null){
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        }
+    };
 }
