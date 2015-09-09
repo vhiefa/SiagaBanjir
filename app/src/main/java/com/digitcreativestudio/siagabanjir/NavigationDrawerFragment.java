e4fa25f6399f1b81640033e748a361227419e815
Eko Wahyudi <eko.wahyudi15@outlook.com>
2015-09-09 21:50:14 +0700

    tambahan lagi

--- app/src/main/java/com/digitcreativestudio/siagabanjir/NavigationDrawerFragment.java
+++ app/src/main/java/com/digitcreativestudio/siagabanjir/NavigationDrawerFragment.java
@@ -17,11 +17,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
